f4a687d33cf0055a36e3aff2b588fd765f4cd55e
Alejandro Macias <alex.mac.fon@gmail.com>
2026-04-18 04:38:44 -0600

    style: prettier

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -2,13 +2,21 @@ import { Module } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { BusinessTransaction, BusinessTransactionSchema } from 'src/mongo-db/schemas/business-transaction.schema';
+import {
+  BusinessTransaction,
+  BusinessTransactionSchema,
+} from 'src/mongo-db/schemas/business-transaction.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{
-    name: BusinessTransaction.name, schema: BusinessTransactionSchema
-  }])],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: BusinessTransaction.name,
+        schema: BusinessTransactionSchema,
+      },
+    ]),
+  ],
   controllers: [TransactionController],
   providers: [TransactionService],
 })
-export class TransactionModule { }
+export class TransactionModule {}
