b51c89f88a5b236ec6ab532f91c0592b8c9dc21d
Alejandro Macias <alex.mac.fon@gmail.com>
2026-04-18 04:40:23 -0600

    fix(transaction entity): imported Llm module

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LlmModule } from 'src/llm/llm.module';
 import {
   BusinessTransaction,
   BusinessTransactionSchema,
@@ -9,6 +10,7 @@ import {
 
 @Module({
   imports: [
+    LlmModule,
     MongooseModule.forFeature([
       {
         name: BusinessTransaction.name,
