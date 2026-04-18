ee1ec15210c046feb2302dca926518ae3430e770
FernandoCG11 <fernando.contreras2143@gmail.com>
2026-04-17 23:15:41 -0600

    feat: add tax profile crud

--- src/tax-profile/dto/create-tax-profile.dto.ts
+++ src/tax-profile/dto/create-tax-profile.dto.ts
@@ -1,1 +1,18 @@
-export class CreateTaxProfileDto {}
+import { IsEnum, IsOptional, IsNumber } from 'class-validator';
+import { RegimenFiscal } from 'src/mongo-db/enums';
+
+export class CreateTaxProfileDto {
+    @IsEnum(RegimenFiscal)
+    regimen_fiscal: RegimenFiscal;
+
+    @IsOptional()
+    last_regimen_update?: Date;
+
+    @IsOptional()
+    @IsNumber()
+    current_salary?: number;
+
+    @IsOptional()
+    @IsNumber()
+    max_income_allowed?: number;
+}
--- src/tax-profile/tax-profile.controller.ts
+++ src/tax-profile/tax-profile.controller.ts
@@ -1,34 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Body,
+  Param,
+} from '@nestjs/common';
+
 import { TaxProfileService } from './tax-profile.service';
 import { CreateTaxProfileDto } from './dto/create-tax-profile.dto';
 import { UpdateTaxProfileDto } from './dto/update-tax-profile.dto';
 
-@Controller('tax-profile')
+@Controller()
 export class TaxProfileController {
-  constructor(private readonly taxProfileService: TaxProfileService) {}
+  constructor(private readonly taxProfileService: TaxProfileService) { }
 
-  @Post()
-  create(@Body() createTaxProfileDto: CreateTaxProfileDto) {
-    return this.taxProfileService.create(createTaxProfileDto);
+  @Post('users/:userId/tax-profile')
+  create(
+    @Param('userId') userId: string,
+    @Body() createDto: CreateTaxProfileDto,
+  ) {
+    return this.taxProfileService.create(userId, createDto);
   }
 
-  @Get()
-  findAll() {
-    return this.taxProfileService.findAll();
+  @Get('users/:userId/tax-profile')
+  findByUser(@Param('userId') userId: string) {
+    return this.taxProfileService.findByUser(userId);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.taxProfileService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTaxProfileDto: UpdateTaxProfileDto) {
-    return this.taxProfileService.update(+id, updateTaxProfileDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.taxProfileService.remove(+id);
+  @Patch('users/:userId/tax-profile')
+  update(
+    @Param('userId') userId: string,
+    @Body() updateDto: UpdateTaxProfileDto,
+  ) {
+    return this.taxProfileService.updateByUser(userId, updateDto);
   }
 }
