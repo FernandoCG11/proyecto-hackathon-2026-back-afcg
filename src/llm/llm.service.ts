25933fe42763f6806481a2ec8b9ab21da17eac9d
Alejandro Macias <alex.mac.fon@gmail.com>
2026-04-18 04:25:10 -0600

    feat: connected transaction service with llm

--- src/llm/llm.service.ts
+++ src/llm/llm.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { GeminiAdapter } from './adapters';
+import { UniversalMessage } from './universal-message.type';
+import { BusinessTransactionDocument } from '../mongo-db/schemas/business-transaction.schema';
 
 @Injectable()
-export class LlmService {}
+export class LlmService {
+  private adapter: GeminiAdapter;
+
+  constructor() {
+    this.adapter = new GeminiAdapter();
+  }
+
+  async extractTransactionData(message: string, file: Express.Multer.File) {
+    const universalMessage: UniversalMessage[] = [
+      {
+        role: 'user',
+        content: 'Analiza el recibo o factura y extrae su informacion',
+        images: [
+          {
+            mimeType: file.mimetype,
+            data: file.buffer.toString('base64'),
+          },
+        ],
+      },
+    ];
+    try {
+      const responseText =
+        await this.adapter.generateResponse(universalMessage);
+      return JSON.parse(responseText) as BusinessTransactionDocument;
+    } catch (err) {
+      const error = err as Error;
+      throw new BadRequestException(error.message.split(`/n`).pop);
+    }
+  }
+}
--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -9,17 +9,19 @@ import {
   BusinessTransaction,
   BusinessTransactionDocument,
 } from 'src/mongo-db/schemas/business-transaction.schema';
+import { LlmService } from '../llm/llm.service';
 
 @Injectable()
 export class TransactionService {
   constructor(
     @InjectModel(BusinessTransaction.name)
     private readonly transactionModel: Model<BusinessTransactionDocument>,
+    private readonly llmService: LlmService,
   ) {}
 
   async create(data: {
     userId: string;
-    file: Express.Multer.File;
+    file?: Express.Multer.File;
     message: string;
   }) {
     const { userId, file, message } = data;
@@ -29,8 +31,13 @@ export class TransactionService {
     }
 
     // 🔥 Aquí después irá IA + OCR
+    const extractedData = await this.llmService.extractTransactionData(
+      message,
+      file,
+    );
 
     const transaction = await this.transactionModel.create({
+      ...extractedData,
       user_id: new Types.ObjectId(userId),
       concept: message, // temporal
       amount: 0, // placeholder
@@ -41,6 +48,7 @@ export class TransactionService {
       id: transaction._id,
       fileName: file.originalname,
       message,
+      extractedData,
     };
   }
 
